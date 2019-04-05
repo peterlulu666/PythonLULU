f4d9eb2ad7e82ddea5afc51718ef3a8053bc3919
peterlulu <peterlulu666@gmail.com>
2019-04-05 18:12:50 -0400

    user info

--- user_info.py
+++ user_info.py
@@ -1,2 +1,21 @@
-print("user")
-print("password")
+psd = "correct"
+
+count_psd = 0
+while count_psd < 3:
+    user_psd = input("type password: ")
+    if user_psd == psd:
+        print("log in")
+        break
+    else:
+        print("type password again")
+    count_psd += 1
+    print(("you have " + str(3 - count_psd) + " chances"))
+
+
+
+
+
+
+
+
+
