450a10bcb3e0dead41b58bcdf2eadbe8ecc25026
peterlulu <peterlulu666@gmail.com>
2019-04-05 18:48:43 -0400

    user info

--- user_info.py
+++ user_info.py
@@ -1,6 +1,10 @@
+#用户最多输入三次密码
+#正确显示登录成功
+#不正确显示错误，还有几次机会
 psd = "correct"
 
 count_psd = 0
+chances = 3
 while count_psd < 3:
     user_psd = input("type password: ")
     if user_psd == psd:
@@ -9,7 +13,7 @@ while count_psd < 3:
     else:
         print("type password again")
     count_psd += 1
-    print(("you have " + str(3 - count_psd) + " chances"))
+    print(("you have " + str(chances - count_psd) + " chances"))
 
 
 
