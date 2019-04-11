f664cf2bdfd78d3965fbd1b29e29950c0edf5fea
peterlulu <peterlulu666@gmail.com>
2019-04-11 10:38:01 -0400

    user info

--- Food Ordering.py
+++ Food Ordering.py
@@ -10,3 +10,7 @@ print(ordering)
 
 for i in ordering:
     print("The price of the " + i[0] + " is " + i[1] + " dollars.")
+
+with open("food.csv", 'w') as f:
+    for i in ordering:
+        f.write(i[0] + ',' + i[1] + '\n')
